54bf8f97e2a8c40fcaab66237939afa4ff21e9f8
unknown <veni.rails@gmail.com>
2014-05-30 00:37:34 +0530

    final copy?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,9 @@ ShoePark::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
+  config.serve_static_assets = true
+config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+config.assets.compile = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
